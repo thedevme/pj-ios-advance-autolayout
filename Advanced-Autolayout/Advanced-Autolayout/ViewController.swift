6df6216ed3f33349e56a0fbdd3569d29bb547d38
Craig Clayton <craig@designtoswiftui.io>
2022-10-27 14:36:45 -0400

    project

---
+++ Advanced-Autolayout/Advanced-Autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Advanced-Autolayout
+//
+//  Created by Craig Clayton on 10/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
